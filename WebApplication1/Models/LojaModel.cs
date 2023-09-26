f9ae61d8bde14b37e048874f23593d8a1077b49f
Luiz Felipe A. Oliveira <67592458+Felip-Oliv@users.noreply.github.com>
2023-09-26 20:13:42 -0300

    Update diagrama
    teset

--- WebApplication1/Models/LojaModel.cs
+++ WebApplication1/Models/LojaModel.cs
@@ -10,6 +10,6 @@ public class Loja
     public int LojaId { get; set; }
     public string? Nome { get; set; }
     public string? Endereco {get; set; }
-    public List<Funcionario> Funcionario { get; set; } = new List<Funcionario>(); // Relação 0 para muitos com Funcionario
+    public List<Funcionario> Funcionario { get; set; } = new List<Funcionario>(); // Relação 1 para muitos com Funcionario
 
 }
