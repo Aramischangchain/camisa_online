aa1381ce24ba62e872a0fa5084bd5b39a4d981ab
Luiz Felipe A. Oliveira <67592458+Felip-Oliv@users.noreply.github.com>
2023-10-03 06:54:34 -0300

    Atualizar o PagamentoModel.cs

--- WebApplication1/Models/PagamentoModel.cs
+++ WebApplication1/Models/PagamentoModel.cs
@@ -4,10 +4,9 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace WebApplication1.Controllers;
 public class Pagamento
 {
-    [Key]
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public int PagamentoId { get; set; }
-    public string? NumeroCartao { get; set; }
-    public string? Validade { get; set; }
-    public string? NomeTitular { get; set; }
+    [Key] [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int PagamentoId { get; set;}
+    public string? NumeroCartao { get; set;}
+    public string? Validade { get; set;}
+    public string? NomeTitular { get; set;}
 }
