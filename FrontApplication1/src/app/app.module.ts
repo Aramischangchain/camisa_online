dd300444d696f3108e307f0397ec092ea859226a
Luiz Felipe A. Oliveira <67592458+Felip-Oliv@users.noreply.github.com>
2023-10-29 19:24:50 -0300

    Update Frontnd
    Adicionando método cadastrar para o cliente.

--- FrontApplication1/src/app/app.module.ts
+++ FrontApplication1/src/app/app.module.ts
@@ -1,22 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { ReactiveFormsModule } from '@angular/forms';
-import { ModalModule } from 'ngx-bootstrap/modal';
+import { ModalModule} from 'ngx-bootstrap/modal';
+
 import { ClienteService } from './cliente.service';
+import { ClienteComponent } from './components/cliente/cliente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClienteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    ModalModule.forRoot()
   ],
-  providers: [],
+  providers: [HttpClientModule, ClienteService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
