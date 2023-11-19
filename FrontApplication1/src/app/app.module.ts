49ad33ffe568b41a2722a006ac2310298e9a33d0
Luiz Felipe A. Oliveira <67592458+Felip-Oliv@users.noreply.github.com>
2023-11-19 17:45:33 -0300

    Add dropdown menu

--- FrontApplication1/src/app/app.module.ts
+++ FrontApplication1/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -14,13 +14,20 @@ import { ClienteService } from './cliente.service';
 import { ClienteComponent } from './components/cliente/cliente.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 
+const routes: Routes = [
+  { path: 'cliente', component: ClienteComponent},
+  { },
+  // Outras rotas...
+];
 @NgModule({
   declarations: [
     AppComponent,
     ClienteComponent,
-    NavBarComponent
+    NavBarComponent,
   ],
+
   imports: [
+    [RouterModule.forRoot(routes)],
     BrowserModule,
     AppRoutingModule,
     CommonModule,
@@ -28,7 +35,10 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
     ReactiveFormsModule,
     ModalModule.forRoot()
   ],
+  exports: [RouterModule],
+
   providers: [HttpClientModule, ClienteService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
